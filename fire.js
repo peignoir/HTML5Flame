9a60f948cc5ede895ba3156ff32adfbcc850e5c1
franck nouyrigat <130941+peignoir@users.noreply.github.com>
2025-05-20 16:16:42 +0300

    Enhance fire effect with mouse interaction

--- fire.js
+++ fire.js
@@ -1,146 +1,97 @@
-/* One day I will invent Fire :) 
+/* One day I will invent Fire :)
 Franck Nouyrigat (@peignoir)*/
- 
-var canvas;  
+
+var canvas;
 var ctx;
+var width;
+var height;
+var Buffer1;
+var Buffer2;
+var mouseX = 0;
+var mouseY = 0;
 
-
-
-Array.matrix = function(m,n, initial) { 
-	var a, i, j, mat =[];
-	for (i = 0; i < m; i += 1) { 
-		a = [];
-		for (j =0; j < n; j += 1) {
-			a[j] = initial; 
-		}
-		mat [i] = a;
-		}	
-		return mat;
-	}
-
-var Buffer1 = Array.matrix(1024, 768, 0);
-var Buffer2 = Array.matrix(1024, 768, 0);
-
+Array.matrix = function(m,n, initial) {
+        var a, i, j, mat =[];
+        for (i = 0; i < m; i += 1) {
+                a = [];
+                for (j =0; j < n; j += 1) {
+                        a[j] = initial;
+                }
+                mat [i] = a;
+                }
+                return mat;
+        };
 
 function fire() {
+    var imageData = ctx.createImageData(width, height);
 
-	
+    // add random heat at the bottom
+    for (var x = 1; x < ( width - 1); x++){
+        var foo = Math.random() * 160;
+        Buffer1[x][(height - 2)] = Buffer1[x][(height - 1)] = foo;
+    }
 
-imageData = ctx.createImageData(width, height);
+    // add extra heat around the mouse position
+    var mx = Math.floor(mouseX);
+    var my = Math.floor(mouseY);
+    for (var dx = -2; dx <= 2; dx++) {
+        for (var dy = -2; dy <= 2; dy++) {
+            var px = mx + dx;
+            var py = my + dy;
+            if(px > 0 && px < width && py > 0 && py < height) {
+                Buffer1[px][py] = 255;
+            }
+        }
+    }
 
+    for (var x = 1; x < (width - 1); x+=1){
+        for (var y = 1; y < (height - 1); y+=1) {
+            if(!Buffer1[x +1]) {Buffer1[x + 1] = 0;}
+            if(x>1 && x < (width-2)) {
+                Buffer2[x][y-1] = (Buffer1[x - 1][y] + Buffer1[x + 1][y] + Buffer1[x][y + 1] + Buffer1[x][y - 1]) / 4 - 1;
+            } else {
+                Buffer2[x][y-1] = (Buffer1[x][y]+ Buffer1[x][y+1])/2 -4;
+            }
+            var val = Buffer2[x][y];
+            var r = val;
+            var g = val * 0.5;
+            var b = 0;
+            setPixel(imageData, x, y, r, g, b, 0xff);
+        }
+    }
+    ctx.putImageData(imageData, 0, 0);
 
-for (x = 1; x < ( width - 1); x++){	 
-	foo = Math.random() * 160;
-	Buffer1[x][(height - 2)] = Buffer1[x][(height - 1)] = Buffer1[x - 1][(height - 2)] = Buffer1[x - 1][(height - 1)] = foo; 
-	 
-	}
-	
-for (x = 1; x < (width - 1); x+=1){
-	//Buffer1[x][(height - 2)] = Math.random() * 250; 
-	//Buffer1[x][(height - 1)] = Math.random() * 250;
-	for (y = 1; y < (height - 1); y+=1)
-	  {		
-		if(!Buffer1[x +1]) {Buffer1[x + 1] = 0;}
-		if(x>1&&x<(width-2)){Buffer2[x][y-1] = (Buffer1[x - 1][y] + Buffer1[x + 1][y] + Buffer1[x][y + 1] + Buffer1[x][y - 1]) / 4 - 1;}
-		else Buffer2[x][y-1] = (Buffer1[x][y]+ Buffer1[x][y+1])/2 -4;
-		setPixel(imageData, x, y, Buffer2[x][y],  0, 0, 0xff); // r, g, b, a
-			}
-	  	}
-
-/*for (x = 0; x < width; x++){
-    for (y = 1; y < height; y++)
-		  {
-		setPixel(imageData, x, y, 0,  Buffer2[x][y], 0, 0xff); // r, g, b, a
-		  }
-		}
-*/
- ctx.putImageData(imageData, 0, 0);
- 
- for (x = 0; x < width; x++){  
-	for (y = 1; y < height; y++)
-	  {		
-	    Buffer1[x][y] = Buffer2[x][y];
-	  }
-	}
+    for (var x = 0; x < width; x++){
+        for (var y = 1; y < height; y++) {
+            Buffer1[x][y] = Buffer2[x][y];
+        }
+    }
 }
 
-
-
 function setPixel(imageData, x, y, r, g, b, a) {
-    index = (x + y * imageData.width) * 4;
+    var index = (x + y * imageData.width) * 4;
     imageData.data[index+0] = r;
     imageData.data[index+1] = g;
     imageData.data[index+2] = b;
     imageData.data[index+3] = a;
 }
 
-/*function setPalette(r,g,b){
-	for (i=0; i<255; i++){
-		Color[i][red] = 0
-		Color[i][green] = 
-		
-	}
-	
-	
-}*/
-
-
-function draw2(){
-width = parseInt(element.getAttribute("width"));
-height = parseInt(element.getAttribute("height"));
-imageData = ctx.createImageData(width, height);
-for (i = 0; i < width * height; i++) {
-	r = parseInt(Math.random() * 256);
-    g = parseInt(Math.random() * 256);
-    b = parseInt(Math.random() * 256);
-	imageData.data[i+0] = 200;
-	imageData.data[i+1] = 200;
-	imageData.data[i+2] = 200;
-	imageData.data[i+3] = 0xff;
-	
-    }    
-}
-
-
-function draw(){
-
-// read the width and height of the canvas
-width = parseInt(element.getAttribute("width"));
-height = parseInt(element.getAttribute("height"));
-// create a new pixel array
-imageData = ctx.createImageData(width, height);
-
-// draw random dots
-for (i = 0; i < width; i++) {
-    x = i;
-	for (j = 60; j < height; j++) {	
-	y = j;
-	r = parseInt(Math.random() * 256);
-    g = parseInt(Math.random() * 256);
-    b = parseInt(Math.random() * 256);
-    setPixel(imageData, x, y, r, g, b, 0xff); // 0xff opaque 
-	setPixel(imageData, x, y-1*i, r, g, b, 0xff); // 0xff opaque
-	}
-}
-
-// copy the image data back onto the canvas
-ctx.putImageData(imageData, 0, 0); // at coords 0,0
-
-}
-
 function clear() {
   ctx.clearRect(0, 0, width, height);
 }
 
-/* lets the magic begins */ 
 function init() {
-  element = document.getElementById("canvas");
-  ctx = element.getContext("2d");
-  width = parseInt(element.getAttribute("width"));
-  height = parseInt(element.getAttribute("height"));
-  //Buffer1 = Array.matrix(width, 200, 0);
-  //Buffer2 = Array.matrix(height, 200, 0);
-  return setInterval(fire, 1);
-
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  width = parseInt(canvas.getAttribute("width"));
+  height = parseInt(canvas.getAttribute("height"));
+  Buffer1 = Array.matrix(width, height, 0);
+  Buffer2 = Array.matrix(width, height, 0);
+  canvas.addEventListener('mousemove', function(e){
+      var rect = canvas.getBoundingClientRect();
+      mouseX = e.clientX - rect.left;
+      mouseY = e.clientY - rect.top;
+  });
+  setInterval(fire, 30);
 }
 
